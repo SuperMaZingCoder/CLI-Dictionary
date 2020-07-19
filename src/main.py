331938751657f60055089c5d28c5e3de9133a42e
Reed Manning <reed.manning06@gmail.com>
2020-07-19 03:57:13 -0400

    Formatted Code

--- src/main.py
+++ src/main.py
@@ -6,10 +6,15 @@ colorit.init_colorit()
 program_running = True
 
 while program_running:
-    w = input(colorit.color("What word would you like to request? (type :quit to quit) ", colorit.Colors.green))
+    w = input(
+        colorit.color(
+            "What word would you like to request? (type :quit to quit) ",
+            colorit.Colors.green,
+        )
+    )
     if w != ":quit":
         w_inf = api.get_word(w)
-        newline = '\n'
+        newline = "\n"
         formatted_inf = (
             f"{colorit.color('Word', colorit.Colors.blue)}: {w_inf['word']}{newline}"
             f"{colorit.color('Is Offensive', colorit.Colors.blue)}: {w_inf['offensive']}{newline}"
