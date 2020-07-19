579857adcd078f2068765ad5c631adec3e61fbd3
Reed Manning <reed.manning06@gmail.com>
2020-07-19 08:26:50 -0400

    Added catch for unidentified words

--- src/main.py
+++ src/main.py
@@ -15,11 +15,21 @@ while program_running:
     if w != ":quit":
         w_inf = api.get_word(w)
         newline = "\n"
-        formatted_inf = (
-            f"{colorit.color('Word', colorit.Colors.blue)}: {w_inf['word']}{newline}"
-            f"{colorit.color('Definitions', colorit.Colors.blue)}"
-            f"{''.join([f'{newline}   {i+1}: {definition}' for i, definition in enumerate(w_inf['definitions'])])}"
-        )
-        print(formatted_inf)
+        if w_inf["return_type"] == "definitions":
+            formatted_res = (
+                f"{colorit.color('Word', colorit.Colors.blue)}: {w_inf['word']}{newline}"
+                f"{colorit.color('Definitions', colorit.Colors.blue)}"
+                f"{''.join([f'{newline}   {i+1}: {definition}' for i, definition in enumerate(w_inf['definitions'])])}"
+            )
+        elif w_inf["return_type"] == "options":
+            if len(w_inf["options"]) > 0:
+                formatted_res = (
+                    f"{colorit.color('Word not found.', colorit.Colors.red)}{newline}"
+                    f"Maybe you meant:"
+                    f"{''.join([f'{newline}   {i}: {option}' for i, option in enumerate(w_inf['options'])])}"
+                )
+            elif len(w_inf["options"]) == 0:
+                formatted_res = f"{colorit.color('Word not found. No similar words were found in the dictionary.', colorit.Colors.red)}{newline}"
+        print(formatted_res)
     else:
         program_running = False
--- src/namespace/download/get_definition.py
+++ src/namespace/download/get_definition.py
@@ -6,25 +6,21 @@ def get_word(word: str) -> list:
     r = requests.get(
         f"https://www.dictionaryapi.com/api/v3/references/sd3/json/{word}?key={apikey.apikey}"
     )
-    json_response = r.json()[0]
-    word = json_response["meta"]["id"]
-    definitions = json_response["shortdef"]
-    word_info = {"word": word, "definitions": definitions}
-    return word_info
+    json_response = r.json()
+    if len(json_response) == 0 or 'meta' not in json_response[0]:
+        return {'return_type': 'options', 'options': [option for option in r.json()[:min(len(r.json()), 5)]]}
+    else:
+        word = json_response[0]["meta"]["id"]
+        definitions = json_response[0]["shortdef"]
+        word_info = {"return_type": "definitions", "word": word, "definitions": definitions}
+        return word_info
 
+def get_json(word):
+    r = requests.get(
+        f"https://www.dictionaryapi.com/api/v3/references/sd3/json/{word}?key={apikey.apikey}"
+    )
+    json_response = r.json()
+    return json_response
 
 if __name__ == "__main__":
-    w = input("What word would you like to request? ")
-    w_inf = get_word(w)
-    formatted_inf = """Word: {word}
-Definitions: \n{definitions}""".format(
-        word=w_inf["word"],
-        offensive=w_inf["offensive"],
-        definitions="".join(
-            [
-                f"   {i+1}: {definition} \n"
-                for i, definition in enumerate(w_inf["definitions"])
-            ]
-        ),
-    )
-    print(formatted_inf)
+    print(get_json(input('What word would you like to request? ')))
