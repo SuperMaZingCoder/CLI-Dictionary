aaf5ef79ee1542e1f01c383421e3943b4af3f8b4
Reed Manning <reed.manning06@gmail.com>
2020-07-19 03:56:58 -0400

    Formatted Code

--- src/main.py
+++ src/main.py
@@ -6,24 +6,15 @@ colorit.init_colorit()
 program_running = True
 
 while program_running:
-    w = input("What word would you like to request? (type :quit to quit) ")
+    w = input(colorit.color("What word would you like to request? (type :quit to quit) ", colorit.Colors.green))
     if w != ":quit":
         w_inf = api.get_word(w)
-        formatted_inf = """
-{colored_word}: {word}
-{colored_offensive}: {offensive}
-{colored_definitions}: \n{definitions}""".format(
-            colored_word=colorit.color("Word", colorit.Colors.blue),
-            colored_offensive=colorit.color("Is Offensive", colorit.Colors.blue),
-            colored_definitions=colorit.color("Definitions", colorit.Colors.blue),
-            word=w_inf["word"],
-            offensive=w_inf["offensive"],
-            definitions="".join(
-                [
-                    f"   {i+1}: {definition} \n"
-                    for i, definition in enumerate(w_inf["definitions"])
-                ]
-            ),
+        newline = '\n'
+        formatted_inf = (
+            f"{colorit.color('Word', colorit.Colors.blue)}: {w_inf['word']}{newline}"
+            f"{colorit.color('Is Offensive', colorit.Colors.blue)}: {w_inf['offensive']}{newline}"
+            f"{colorit.color('Definitions', colorit.Colors.blue)}"
+            f"{''.join([f'{newline}   {i+1}: {definition}' for i, definition in enumerate(w_inf['definitions'])])}"
         )
         print(formatted_inf)
     else:
